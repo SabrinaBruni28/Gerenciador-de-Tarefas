e6a42a761d001e144b8a7c1db34e4eac9ef93482
SabrinaBruni28 <sabrina.b.faria@ufv.br>
2026-01-12 22:39:56 -0300

    Tela detalhes funcionando

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,15 @@
 import AddTask from "./components/AddTask";
 import Tasks from "./components/Tasks";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      title: "Estudar React",
-      day: "12 de janeiro de 2026 às 10:00",
-      description: "Ler a documentação oficial e fazer exercícios práticos.",
-      isCompleted: false,
-    },
-    {
-      id: 2,
-      title: "Fazer compras",
-      day: "13 de janeiro de 2026 às 15:00",
-      description: "Comprar mantimentos para a semana.",
-      isCompleted: false,
-    },
-    {
-      id: 3,
-      title: "Ir ao médico",
-      day: "14 de janeiro de 2026 às 09:00",
-      description: "Consulta anual de rotina.",
-      isCompleted: false,
-    },
-  ]);
+  const [tasks, setTasks] = useState(
+    JSON.parse(localStorage.getItem("tasks")) || []
+  );
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
 
   function onTaskClick(taskId) {
     const newTasks = tasks.map((task) => {
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,6 +1,16 @@
 import { ChevronRightIcon, TrashIcon } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
-function Tasks({tasks, onTaskClick, onDeleteTaskClick}) {
+function Tasks({ tasks, onTaskClick, onDeleteTaskClick }) {
+  const navigate = useNavigate();
+
+  function onSeeDetailsClick(task) {
+    const queryParams = new URLSearchParams();
+    queryParams.set("title", task.title);
+    queryParams.set("description", task.description);
+    navigate(`/task?${queryParams.toString()}`);
+  }
+
   return (
     <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
       {tasks.map((task) => (
@@ -12,7 +22,10 @@ function Tasks({tasks, onTaskClick, onDeleteTaskClick}) {
           >
             {task.title}
           </button>
-          <button className="bg-slate-400 p-2 rounded-md text-white">
+          <button
+            onClick={() => onSeeDetailsClick(task)}
+            className="bg-slate-400 p-2 rounded-md text-white"
+          >
             <ChevronRightIcon />
           </button>
           <button
