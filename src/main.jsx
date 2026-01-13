b43fad69c4d58b3bb45ccfe544fb2dc168647461
SabrinaBruni28 <sabrina.b.faria@ufv.br>
2026-01-13 17:13:46 -0300

    Ajuste de Rotas

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import {
+  createHashRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+import App from "./App.jsx";
 import TaskPage from "./pages/TaskPage.jsx";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <App />,
